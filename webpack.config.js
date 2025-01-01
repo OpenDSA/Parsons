e6e7eee03ae6b8674d01263ef1ffd7daaf4981da
Biritwum-Nyarko Kwasi Manu <51910665+kwasimbnyarko@users.noreply.github.com>
2025-01-01 05:07:26 -0500

    merge

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path'), webpack = require('webpack');
+
+module.exports = {
+    entry: './src/lib/parsons.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode:'development',
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "less-loader",
+                        options: {
+                            lessOptions: {
+                                strictMath: true,
+                            },
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins:[ new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+    })]
+};
