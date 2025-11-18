d7eb89677bb9fb7fe62277d0955c689a61d8651d
prakharmodi26 <prakharmodi25@gmail.com>
2025-11-17 23:54:21 -0500

    npm fixes

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const path = require('path');
 
-const distPath = path.join(__dirname, 'dist', 'parsons.js');
-const parsonsBundle = require(distPath);
-const Parsons = parsonsBundle.default || parsonsBundle;
-
 const { parsePIF } = require('./server/helpers/pifParsingHelpers');
 
+let cachedParsons;
+
+function loadParsonsBundle() {
+  if (cachedParsons) {
+    return cachedParsons;
+  }
+
+  if (typeof window === 'undefined' || typeof document === 'undefined') {
+    throw new Error(
+      'Parsons UI bundle requires a browser environment. ' +
+      'Provide window/document (e.g., via JSDOM) before accessing Parsons.'
+    );
+  }
+
+  const distPath = path.join(__dirname, 'dist', 'parsons.js');
+  const bundle = require(distPath);
+  cachedParsons = bundle.default || bundle;
+  return cachedParsons;
+}
+
 module.exports = {
-  Parsons,
+  get Parsons() {
+    return loadParsonsBundle();
+  },
   parsePIF,
+  loadParsonsBundle,
 };
